ce7141444e3039d4077453a9f4970e4341657ec5
Hai Trinh <hai.trinh@abat.de>
2025-05-05 14:25:47 +0200

    basic layout for play view

--- webapp/controller/Play.controller.js
+++ webapp/controller/Play.controller.js
@@ -1,18 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ], 
 /**
  * 
  * @param {typeof sap.ui.core.mvc.Controller} Controller 
+ * @param {typeof sap.ui.model.json.JSONModel} JSONModel
  */
-function(Controller) {
+function(Controller, JSONModel) {
     "use strict";
 
     Controller.extend("de.abatgroup.blackjackui5.controller.Play", {
+        onInit: function(){
+            let imgModel = new JSONModel(sap.ui.require.toUrl("de/abatgroup/blackjackui5/model/img.json"));
+            this.getView().setModel(imgModel, "img");
+        },
+
         onUsername: function(){
             sap.ui.require(["sap/ushell/Container"], async function (Container) {
                 const userInfo = await Container.getServiceAsync("UserInfo");
                 const userId = userInfo.getId();
+                // @ts-ignore
                 sap.m.MessageBox.show(userId);
               });
             
