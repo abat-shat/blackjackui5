7f7dc7afffe8870fa7441e75cde921f6aeaa1a53
Hai Trinh <hai.trinh@abat.de>
2025-06-03 11:53:26 +0200

    housekeeping

--- webapp/controller/Highscore.controller.js
+++ webapp/controller/Highscore.controller.js
@@ -1,26 +1,17 @@
 sap.ui.define([
     "./BaseController",
-    "sap/ui/model/json/JSONModel",
-    "sap/m/MessageBox",
     "sap/ui/model/Sorter"
 ], 
 /**
  * 
  * @param {typeof sap.ui.core.mvc.Controller} Controller
- * @param {typeof sap.ui.model.json.JSONModel} JSONModel
- * @param {typeof sap.m.MessageBox} MessageBox 
  */
-function(Controller, JSONModel, MessageBox, Sorter) {
+function(Controller, Sorter) {
     "use strict";
 
     Controller.extend("de.abatgroup.blackjackui5.controller.Highscore", {
         onInit: function(){
 
-        },
-
-        onPresser: function() {
-            const list = this.byId("highscoreTable").getBinding("items");
-            debugger;
         },
 
         sortPlayers: function(){
