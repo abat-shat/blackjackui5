86a07592aad28eed8069d969602a153d6d9803ab
Hai Trinh <hai.trinh@abat.de>
2025-06-04 10:22:43 +0200

    Your Balance for each player

--- webapp/controller/Highscore.controller.js
+++ webapp/controller/Highscore.controller.js
@@ -5,13 +5,15 @@ sap.ui.define([
 /**
  * 
  * @param {typeof sap.ui.core.mvc.Controller} Controller
+ * @param {typeof sap.ui.model.Sorter} Sorter
  */
 function(Controller, Sorter) {
     "use strict";
 
     Controller.extend("de.abatgroup.blackjackui5.controller.Highscore", {
         onInit: function(){
-
+            let variable = this.byId("hsSfLabel1");
+            console.log(variable);
         },
 
         sortPlayers: function(){
@@ -20,6 +22,18 @@ function(Controller, Sorter) {
             sorters.push(coinSorter);
 
             this.byId("highscoreTable").getBinding("items").sort(sorters);
+        },
+
+        onRefreshBalance: function(){
+            const oDataModel = this.getOwnerComponent().getModel();
+            const oContext = oDataModel.bindContext("/Coin('" + this.username() + "')");
+            oContext.requestObject().then(this._bindContextToSimpleForm.bind(this));
+        },
+        _bindContextToSimpleForm: function(oData) {
+            this.getView().byId("hsSFLabel2").setText(oData.AbatCoin + " abatCoins");
+            this.getView().byId("hsSFLabel3").setText(oData.Bonus + " Bonus");
         }
+
+        
     });
 });
