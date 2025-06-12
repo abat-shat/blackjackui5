9824affe4ad95a278291f8ca54ea5ded4c794b7f
Hai Trinh <hai.trinh@abat.de>
2025-06-12 11:20:11 +0200

    Template for shop

---
+++ webapp/controller/Shop.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "./BaseController"
+], function(Controller) {
+    "use strict";
+
+    return Controller.extend("de.abatgroup.blackjackui5.controller.Shop", {
+        onInit: function() {
+        }
+    });
+});
